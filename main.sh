4f1cbd5012c4e1e62135fe082f7e961fe72cf909
Hazemhassan0 <hazemabdelnasser0@gmail.com>
2025-09-16 21:38:18 +0300

    Initial commit

---
+++ main.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+# Importing modules
+source ./tables.sh
+source ./insert.sh
+source ./update.sh
+
+# ========== Database Management ==========
+# Author: Hazem Abdelnasser  
+# Responsibilities:
+# - Main menu
+# - Create/List/Drop/Connect databases
+# - DB Folder structure setup
+
+DB_PATH="./databases"
+
+mkdir -p "$DB_PATH"
+
+create_db() {
+    read -p "Enter the new database name please: " db_name
+
+    if [[ -z "$db_name" ]]; then
+        echo "Error: Database name cannot be empty."
+        return
+    fi
+    if [[ "$db_name" =~ [^a-zA-Z0-9_] ]]; then
+        echo "Error: Database name can only contain letters, numbers, and underscores."
+        return
+    fi
+
+    if [[ -d "$DB_PATH/$db_name" ]]; then
+        echo "Error: Database '$db_name' already exists."
+    else
+        mkdir -p "$DB_PATH/$db_name"
+        echo "Database '$db_name' was created successfully."
+    fi
+
+
+
+}
+
+list_dbs() {
+    echo "Available Databases:"
+    ls "$DB_PATH"
+}
+
+drop_db() {
+    read -p "Enter the database name you want to drop: " db_name
+
+    if [[ -z "$db_name" ]]; then
+        echo "Error: Database name cannot be empty."
+        return
+    fi
+
+    if [[ -d "$DB_PATH/$db_name" ]]; then
+        rm -rf "$DB_PATH/$db_name"
+        echo "Database '$db_name' was dropped successfully."
+    else
+        echo "Error: Database '$db_name' does not exist."
+    fi
+}
+
+
+connect_db() {
+    read -p "Enter the database name you want to connect to: " db_name
+
+    if [[ -z "$db_name" ]]; then
+        echo "Error: Database name cannot be empty."
+        return
+    fi
+
+    if [[ -d "$DB_PATH/$db_name" ]]; then
+        echo "Connected to database '$db_name'."
+        DB_DIR="$DB_PATH/$db_name" # variable to hold the current database path, export if needed or just pass it to functions
+        db_menu    #  ya beshooo Call the database menu function from tables.sh
+    else
+        echo "Error: Database '$db_name' does not exist."
+    fi
+}
+
+main_menu() {
+    while true; do
+        echo "=============================="
+        echo " Database Management System"
+        echo "=============================="
+        echo "========== Main Menu =========="
+        echo "=============================="
+
+        echo "1. Create Database"
+        echo "2. List Databases"
+        echo "3. Drop Database"
+        echo "4. Connect to Database"
+        echo "5. Exit"
+        read -p "Choose an option (1-5): " choice
+
+        case $choice in
+            1) create_db ;;
+            2) list_dbs ;;
+            3) drop_db ;;
+            4) connect_db ;;
+            5) echo "Exiting..."; exit 0 ;;
+            *) echo "Invalid option. Please try again (if you want to exit, choose 5 ,please)." ;;
+        esac
+    done
+}
+
+# initialize program
+main_menu
